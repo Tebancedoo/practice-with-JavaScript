5910e6963184ea7069a4388e3704cd4621121814
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-11-20 14:59:15 -0500

    Update 04_script.js

--- JavaScript/06_DOM/js/04_script.js
+++ JavaScript/06_DOM/js/04_script.js
@@ -15,27 +15,27 @@ console.log(noExiste);
 //InnerText
 /*Es una propiedad que nos permite cambiar o modificar el contenido de un elemento de texto o consultar su valor. */
 
-function recuperarTitulo() {
+/*function recuperarTitulo() {
     alert(document.getElementById("titulo1").innerText)
-}
+}*/
 
 //TextContent
 /*Es una propieda que nos permite representar el contenido de un nodo y sus decendiente*/
 
-var text = Node.textContent;
-Node.textContent = string;
+/*var text = Node.textContent;
+Node.textContent = string;*/
 
 //InnerHTML
 /*Es una propiedad que nos deja renornar el HTML, permite leer un dato o asignarlo a contenido de un div, o bien del mismo control. Permitiendo asi asignarle valores a los controles */
 
 
-<div id ="DivContenedor"><h1>Texto inicial</h1>
+/*<div id ="DivContenedor"><h1>Texto inicial</h1>
 
     
 <script>
     var element = document.getElementById("DivContenedor");
     element.innerHTML = "Texto final";
-</script>
+</script>*/
 
 //Funciones dom
 
