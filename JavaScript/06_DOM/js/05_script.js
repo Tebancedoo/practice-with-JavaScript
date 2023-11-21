858e316bc9bdec14b4e1a597f6694e0dbf1de226
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-11-21 11:14:53 -0500

    Add files via upload

--- JavaScript/06_DOM/js/05_script.js
+++ JavaScript/06_DOM/js/05_script.js
@@ -6,9 +6,9 @@ console.log(encabezado.textContent);//visibility: hidden, si lo encuentra
 console.log(encabezado.innerHTML);*/
 
 //agregar modificaciones
-const nuevoHeading = 'Nuevo Heading'
+/*const nuevoHeading = 'Nuevo Heading'
 document.querySelector('.contenido-hero h1').textContent = nuevoHeading;
-console.log(encabezado);
+console.log(encabezado);*/
 
 const imagen = document.querySelector('.card img');
 imagen.src = 'img/hacer2.jpg';
@@ -20,4 +20,14 @@ encabezado.style.backgroundColor = 'red';
 encabezado.style.fontFamily = 'Arial';
 encabezado.style.textTransform= 'uppercase';
 encabezado.style.fontSize= '36px';
+
+
+//crear enlace
+const enlace = document.createElement('A');
+enlace.textContent = 'Nuevo Enlace';
+enlace.href = '/nuevo_enlace';
+console.log(enlace);
+
+
+//
 
