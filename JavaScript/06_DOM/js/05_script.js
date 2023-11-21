afbca6a0a4275042d4619491236045a20f4d6a5b
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-11-21 11:06:33 -0500

    Add files via upload

---
+++ JavaScript/06_DOM/js/05_script.js
@@ -0,0 +1,23 @@
+
+/*const encabezado = document.querySelector('.contenido-hero h1').textContent = 'nuevo head';
+console.log (encabezado);//agregar modificaciones
+console.log(encabezado.InnerText);//visibility: hidden, no lo encuentra
+console.log(encabezado.textContent);//visibility: hidden, si lo encuentra
+console.log(encabezado.innerHTML);*/
+
+//agregar modificaciones
+const nuevoHeading = 'Nuevo Heading'
+document.querySelector('.contenido-hero h1').textContent = nuevoHeading;
+console.log(encabezado);
+
+const imagen = document.querySelector('.card img');
+imagen.src = 'img/hacer2.jpg';
+
+
+//cambiar elementos del estilo del h1
+const encabezado = document.querySelector('h1');
+encabezado.style.backgroundColor = 'red';
+encabezado.style.fontFamily = 'Arial';
+encabezado.style.textTransform= 'uppercase';
+encabezado.style.fontSize= '36px';
+
